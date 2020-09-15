23565efffa4c7f34966a9f9ac6147ddcc809cd5e
Charis Moutafidis <charismoutafaidis@gmail.com>
2020-09-15 14:26:19 +0300

    Added validations on todo fields

--- src/main/java/com/inspectus/entity/Todo.java
+++ src/main/java/com/inspectus/entity/Todo.java
@@ -6,7 +6,12 @@
 package com.inspectus.entity;
 
 import java.time.LocalDate;
+import javax.json.bind.annotation.JsonbDateFormat;
 import javax.persistence.*;
+import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 /**
  *
@@ -19,8 +24,16 @@ public class Todo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @NotEmpty(message = "Task must be set")
+    @Size(min = 10, message = "Task should not be less than 10 characters")
     private String task;
+
+    @NotNull(message = "Due date must be set")
+    @FutureOrPresent(message = "Due date must be in the present or future")
+    @JsonbDateFormat(value = "yyyy-MM-dd")
     private LocalDate dueDate;
+    
     private boolean isCompleted;
     private LocalDate dateCompleted;
     private LocalDate dateCreated;
